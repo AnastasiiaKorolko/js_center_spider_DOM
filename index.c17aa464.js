af100ede9a0f1167b6ea968f525219a7a2518fe3
Korolko Anastasiia <korolkonasta@gmail.com>
2024-07-30 17:11:47 +0300

    make build

---
+++ index.c17aa464.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".spider"),t=document.querySelector(".wall"),n=t.clientWidth,c=t.clientHeight,i=e.clientHeight,l=e.clientWidth;t.style.position="relative",e.style.top="".concat((c-i)/2,"px"),e.style.left="".concat((n-l)/2,"px")});
+//# sourceMappingURL=index.c17aa464.js.map
